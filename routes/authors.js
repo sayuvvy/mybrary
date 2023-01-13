76737fcf9faf0e42c1de68c6ed3f2de2e0f94267
Sayuvvy1982 <sayuvvy@gmail.com>
2023-01-13 07:51:38 +0530

    Routes added for author

---
+++ routes/authors.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const author = require('../models/author')
+const Author = require('../models/author')
+const router = express.Router()
+
+
+router.get('/', async (req, res) => {    
+    let searchOptions = {}
+
+    if(req.query.name != null && req.query.name !== '' ) {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {authors: authors, searchOptions: req.query})
+    }
+    catch (err)
+    {
+        res.redirect('authors/index', {
+            errorMessage: `Error retrieving authors`
+        })
+    }
+    
+})
+
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author()})    
+})
+
+router.post('/', async (req, res) => {    
+
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try {
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    }
+    catch (err)
+    {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: `Error creating author : ${author.name}`
+        })
+    }
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
@@ -19,9 +20,13 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', ()=> console.log('connected successfully to mongoose'))
 
+app.use(express.urlencoded())
+app.use(express.json())
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
